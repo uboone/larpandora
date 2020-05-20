05a7a1e09f9ce6ce954bbdfa742a663a9f822bd6
Etienne Chardonnet <echardon@dunegpvm04.fnal.gov>
2020-05-20 04:15:12 -0500

    Added unpolished LEM gaps in LArPandora.Input.cxx

--- larpandora/LArPandoraInterface/LArPandoraInput.cxx
+++ larpandora/LArPandoraInterface/LArPandoraInput.cxx
@@ -276,13 +276,13 @@ void LArPandoraInput::CreatePandoraReadoutGaps(const Settings &settings, const L
             {
                 const geo::PlaneGeo &plane(TPC.Plane(iplane));
                 const float halfWirePitch(0.5f * theGeometry->WirePitch(plane.View()));
-                const unsigned int nWires(theGeometry->Nwires(geo::PlaneID(icstat, itpc, plane.View())));
+                const unsigned int nWires(theGeometry->Nwires(plane.ID()));
 
                 int firstBadWire(-1), lastBadWire(-1);
 
                 for (unsigned int iwire = 0; iwire < nWires; ++iwire)
                 {
-                    const raw::ChannelID_t channel(theGeometry->PlaneWireToChannel(plane.View(), iwire, itpc, icstat));
+                    const raw::ChannelID_t channel(theGeometry->PlaneWireToChannel(iplane, iwire, itpc, icstat));
                     const bool isBadChannel(channelStatus.IsBad(channel));
                     const bool isLastWire(nWires == (iwire + 1));
 
@@ -320,15 +320,24 @@ void LArPandoraInput::CreatePandoraReadoutGaps(const Settings &settings, const L
                             parameters.m_lineEndX = volumeIter->second.GetCenterX() + 0.5f * volumeIter->second.GetWidthX();
                         }
 
-                        const geo::View_t iview = (geo::View_t)iplane;
+                        const geo::View_t iview = (geo::View_t)plane.View();
                         const geo::View_t pandoraView(LArPandoraGeometry::GetGlobalView(icstat, itpc, iview));
 
-                        if (pandoraView == geo::kW || pandoraView == geo::kY)
+                        if (pandoraView == geo::kW || pandoraView == geo::kZ)
                         {
                             const float firstW(firstXYZ[2]);
                             const float lastW(lastXYZ[2]);
 
-                            parameters.m_lineGapType = pandora::TPC_WIRE_GAP_VIEW_W;
+                            parameters.m_lineGapType = pandora::TPC_WIRE_GAP_VIEW_U;
+                            parameters.m_lineStartZ = std::min(firstW, lastW) - halfWirePitch;
+                            parameters.m_lineEndZ = std::max(firstW, lastW) + halfWirePitch;
+                        }
+                        if (pandoraView == geo::kY)
+                        {
+                            const float firstW(firstXYZ[1]);
+                            const float lastW(lastXYZ[1]);
+
+                            parameters.m_lineGapType = pandora::TPC_WIRE_GAP_VIEW_V;
                             parameters.m_lineStartZ = std::min(firstW, lastW) - halfWirePitch;
                             parameters.m_lineEndZ = std::max(firstW, lastW) + halfWirePitch;
                         }
